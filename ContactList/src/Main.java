543f614c2ab5eb0f1d37dd107b4e881f022d5d82
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-03-16 16:26:30 -0300

    refactor: adding contacts by FilaContatoCliens and ignore phone numbers if they are null

--- ContactList/src/Main.java
+++ ContactList/src/Main.java
@@ -15,63 +15,80 @@ public class Main {
         FilaContatoClientes contatoCallCenter = new FilaContatoClientes();
         /** Declaração da classe Scanner para entrada de dados */
         Scanner ler = new Scanner(System.in);
-        /** estrutura de repetição while contendo as opções */
+        /** Estrutura de repetição while contendo as opções */
         while (true) {
             System.out.println("Selecione as seguintes opções");
             System.out.println("[1] - |Adicionar contato|");
             System.out.println("[2] - |Próximo contato|");
             System.out.println("[3] - |Listar contatos|");
             System.out.println("[4] - |Sair do programa|");
-            String nome, numeroTelefoneCasa, numeroCelular, numeroTelefoneTrabalho = null;
             int option = ler.nextInt();
 
             /** Estrutura de condição para o usuário efetuar a opção escolhida, ou sair. */
             switch (option) {
-                case 1:
+                case 1: // Adicionar contato
+                    String nome, numeroTelefoneCasa, numeroCelular, numeroTelefoneTrabalho = null;
                     System.out.println("|Adicionar Contato|");
                     System.out.println("Nome do contato:");
-                    ler.nextLine(); // consume line leftover
+                    ler.nextLine(); // consumir sobras de linha
 
-                    nome = String.valueOf(ler.nextLine());
+                    nome = ler.nextLine();
                     do {
                         System.out.println(
-                                "Numero de telefone residencial (Ex: (61) xxxx-xxxx):");
+                                "Numero de telefone residencial.\n"
+                                        + "(Ex.: (99) XXXX-XXXX ou (99) 9XXXX-XXXX):\n"
+                                        + "Caso não exista, deixe nulo.");
                         numeroTelefoneCasa = ler.nextLine();
-                    } while (!contatoCallCenter.autenticarNumeroTelefone(numeroTelefoneCasa));
+                    } while (!Contato.autenticarNumeroTelefone(numeroTelefoneCasa)
+                            || numeroTelefoneCasa == null);
 
                     do {
                         System.out.println(
-                                "Numero de celular (Ex: (61) xxxxx-xxxx):");
+                                "Numero de celular.\n"
+                                        + "(Ex.: (99) XXXX-XXXX ou (99) 9XXXX-XXXX):\n"
+                                        + "Caso não exista, deixe nulo.");
                         numeroCelular = ler.nextLine();
-                    } while (!contatoCallCenter.autenticarNumeroTelefone(numeroCelular));
+                    } while (!Contato.autenticarNumeroTelefone(numeroCelular)
+                            || numeroCelular == null);
 
                     do {
                         System.out.println(
-                                "Numero de telefone de trabalho (Ex: (61) xxxxx-xxxx):");
+                                "Numero de telefone de trabalho.\n"
+                                        + "(Ex.: (99) XXXX-XXXX ou (99) 9XXXX-XXXX):\n"
+                                        + "Caso não exista, deixe nulo.");
                         numeroTelefoneTrabalho = ler.nextLine();
-                    } while (!contatoCallCenter.autenticarNumeroTelefone(numeroTelefoneTrabalho));
+                    } while (!Contato.autenticarNumeroTelefone(numeroTelefoneTrabalho)
+                            || numeroTelefoneTrabalho == null);
 
-                    contatoCallCenter.adicionarContato(nome, numeroTelefoneCasa, numeroCelular, numeroTelefoneTrabalho);
+                    Contato contato = Contato.adicionarContatos(nome, numeroCelular, numeroTelefoneCasa,
+                            numeroTelefoneTrabalho);
+                    contatoCallCenter.adicionarContatos(contato);
                     break;
-                case 2:
-                    if (!contatoCallCenter.removerContato()) {
-                        System.out.println("\n===Fila vazia===\n");
+
+                case 2: // Próximo contato
+                    Contato tmp = contatoCallCenter.proximoContato();
+                    if (tmp == null) {
+                        System.out.println("Fila vazia");
                     } else {
-                        System.out.println("Próximo Contato:");
-                        contatoCallCenter.proximoContato();
+                        System.out.println("Nome: " + tmp.getNome());
+                        System.out.println("Número de celular: " + tmp.getNumeroCelular());
+                        System.out.println("Número de telefone residencial: " + tmp.getNumeroTelefoneCasa());
+                        System.out.println("Número de telefone do trabalho: " + tmp.getNumeroTelefoneTrabalho());
                     }
                     break;
 
-                case 3:
-                    contatoCallCenter.listarContato();
+                case 3: // Listar contatos
+                    System.out.println("Lista de contatos");
+                    contatoCallCenter.listarContatos();
                     break;
 
-                case 4:
+                case 4: // Sair do programa
                     System.out.println("\n===Você encerrou a lista de contatos.===");
+                    ler.close();
                     System.exit(0);
                     break;
 
-                default:
+                default: // Em caso de opção desconhecida, executa default
                     System.out.println(
                             "Opção não reconhecida. Favor selecionar uma das opções disponíveis");
                     break;
