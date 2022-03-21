50586cb626d1b827c33534ff7939f590f2319273
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2022-03-21 16:43:27 -0300

    Add initial Disciplina class

---
+++ GradeControl/src/sistema/Disciplina.java
@@ -0,0 +1,76 @@
+package sistema;
+
+public class Disciplina {
+  private String codigo;
+  private String nome;
+  private String descricao;
+  private String turno;
+  private float cargaHoraria;
+  private int periodo;
+  private String modalidade;
+  private String docente;
+
+  public String getCodigo() {
+    return codigo;
+  }
+
+  public void setCodigo(String codigo) {
+    this.codigo = codigo;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public String getTurno() {
+    return turno;
+  }
+
+  public void setTurno(String turno) {
+    this.turno = turno;
+  }
+
+  public float getCargaHoraria() {
+    return cargaHoraria;
+  }
+
+  public void setCargaHoraria(float cargaHoraria) {
+    this.cargaHoraria = cargaHoraria;
+  }
+
+  public int getPeriodo() {
+    return periodo;
+  }
+
+  public void setPeriodo(int periodo) {
+    this.periodo = periodo;
+  }
+
+  public String getModalidade() {
+    return modalidade;
+  }
+
+  public void setModalidade(String modalidade) {
+    this.modalidade = modalidade;
+  }
+
+  public String getDocente() {
+    return docente;
+  }
+
+  public void setDocente(String docente) {
+    this.docente = docente;
+  }
+}
